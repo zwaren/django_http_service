678c658fb2e4f87c71dac8e7e48e0bb3d6b10676
zwaren <zwaren@yandex.ru>
2019-09-03 19:51:16 +0400

    Add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Street(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    street = models.ForeignKey(Street, on_delete=models.CASCADE)
+    house = models.CharField(max_length=200)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
--- django_http_service/settings.py
+++ django_http_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'django_http_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
