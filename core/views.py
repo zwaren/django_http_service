f1d18f6056639fa7711edcd5671bbedd11e12401
zwaren <zwaren@yandex.ru>
2019-09-25 19:54:45 +0400

    Replace get method with get_queryset

--- core/views.py
+++ core/views.py
@@ -18,10 +18,10 @@ class StreetList(ListAPIView):
     queryset = Street.objects.all()
     serializer_class = StreetSerializer
 
-    def get(self, request, format=None):
-        streets = self.get_queryset()
+    def get_queryset(self):
+        streets = self.queryset
 
-        city_id = request.GET.get('city_id')
+        city_id = self.request.GET.get('city_id')
         if city_id:
             try:
                 City.objects.get(id=city_id)
@@ -29,18 +29,17 @@ class StreetList(ListAPIView):
                 raise CityDoesntExistException()
             streets = streets.filter(city=city_id)
 
-        serializer = self.get_serializer_class()(streets, many=True)
-        return Response(serializer.data)
+        return streets
 
 
 class ShopList(ListCreateAPIView):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
 
-    def get(self, request, format=None):
-        shops = self.get_queryset()
+    def get_queryset(self):
+        shops = self.queryset
 
-        street_name = request.GET.get('street')
+        street_name = self.request.GET.get('street')
         if street_name:
             try:
                 Street.objects.get(name=street_name)
@@ -48,7 +47,7 @@ class ShopList(ListCreateAPIView):
                 raise StreetDoesntExistException()
             shops = shops.filter(street__name=street_name)
 
-        city_name = request.GET.get('city')
+        city_name = self.request.GET.get('city')
         if city_name:
             try:
                 City.objects.get(name=city_name)
@@ -56,12 +55,11 @@ class ShopList(ListCreateAPIView):
                 raise CityDoesntExistException()
             shops = shops.filter(city__name=city_name)
 
-        is_open = request.GET.get('open')
+        is_open = self.request.GET.get('open')
         nowtime = timezone.now().time()
         if is_open == '1':
             shops = shops.filter(Q(open_time__lte=nowtime) & Q(close_time__gte=nowtime))
         elif is_open == '0':
             shops = shops.filter(Q(open_time__gt=nowtime) | Q(close_time__lt=nowtime))
 
-        serializer = self.get_serializer_class()(shops, many=True)
-        return Response(serializer.data)
+        return shops
